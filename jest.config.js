50475b9b269bf81e74340bab1d368e8f86b56d7e
Mark Murray <mark.murray01@bbc.co.uk>
2018-08-06 07:38:07 +0100

    Resolve path to ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 const { existsSync } = require('fs')
 const { join } = require('path')
+const resolve = require('resolve')
 
 const hasSetupFile = existsSync(join(process.cwd(), 'test', 'setup.js'))
 const { createTypescriptConfigFile } = require('scripts-toolbox')
@@ -20,7 +21,7 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testURL: 'http://localhost',
   transform: {
-    '.(ts|tsx)': join(__dirname, 'node_modules', 'ts-jest'),
+    '.(ts|tsx)': resolve.sync('ts-jest', { basedir: __dirname }),
   },
   globals: {
     'ts-jest': {
