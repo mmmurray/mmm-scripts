40587e0654ccdf59540db820e82f3b8124381138
Mark Murray <mark@murray.xyz>
2018-12-02 16:19:42 +0000

    control watch mode tasks

--- bin/mmm.js
+++ bin/mmm.js
@@ -25,34 +25,54 @@ const exists = path => existsSync(join(process.cwd(), path))
 const devServerScript = 'lib/server/dev.js'
 const useDevServer = exists(devServerScript)
 
-const webpackEntryPaths = ['./src/app/index.tsx', './src/client/index.tsx', './src/index.tsx']
+const webpackEntryPaths = [
+  './src/app/index.tsx',
+  './src/client/index.tsx',
+  './src/index.tsx',
+]
 const webpackEntry = webpackEntryPaths.find(exists)
-const webpackConfig = webpackConfigBuilder.default({ proxy: useDevServer, entry: webpackEntry })
+const webpackConfig = webpackConfigBuilder.default({
+  proxy: useDevServer,
+  entry: webpackEntry,
+})
 
 run({
   jest: args => jest({ config: jestConfig, options: { watch: true, ...args } }),
-  coverage: args => jest({ config: jestConfig, options: { coverage: true, ...args } }),
+  coverage: args =>
+    jest({ config: jestConfig, options: { coverage: true, ...args } }),
   build: () => typescriptBuild({ config: tsConfig }),
   pack: () => webpackBuild({ config: webpackConfig, env: 'production' }),
   format: () => prettier({ config: prettierConfig }),
   watch: args => {
-    const server = useDevServer
-      ? nodemonWatch({
-          options: {
-            script: devServerScript,
-            delay: 1,
-            watch: 'lib/server',
-          },
-        })
-      : null
+    const use = (args.use || '').split(',')
+    const useBundler = use.length === 0 || use.includes('bundler')
+    const useServer = use.length === 0 || use.includes('server')
+    const useCompiler = use.length === 0 || use.includes('compiler')
+
+    const server =
+      useDevServer && useServer
+        ? nodemonWatch({
+            options: {
+              script: devServerScript,
+              delay: 1,
+              watch: 'lib/server',
+            },
+          })
+        : null
 
     return watcher({
-      compile: typescriptWatch({ config: tsConfig }),
-      bundler: webpackWatch({
-        config: webpackConfig,
-        env: 'development',
-        port: args.webpackPort,
-      }),
+      ...(useCompiler
+        ? { compile: typescriptWatch({ config: tsConfig }) }
+        : {}),
+      ...(useBundler
+        ? {
+            bundler: webpackWatch({
+              config: webpackConfig,
+              env: 'development',
+              port: args.webpackPort,
+            }),
+          }
+        : {}),
       ...(server ? { server } : {}),
     })
   },
