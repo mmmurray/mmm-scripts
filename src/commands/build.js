a48824094e1aff7dc7827a7b2974dd6c49ea3473
Mark Murray <mark@murray.xyz>
2019-06-22 18:53:38 +0100

    feat(build): empty lib before build

--- src/commands/build.js
+++ src/commands/build.js
@@ -1,7 +1,10 @@
+const { emptyDir } = require('fs-extra')
+const { join } = require('path')
 const getBinPath = require('../helpers/bin-path')
 const spawn = require('../helpers/spawn')
 
 const build = async projectRoot => {
+  await emptyDir(join(projectRoot, 'lib'))
   await spawn(getBinPath('tsc'), [], projectRoot)
 }
 
