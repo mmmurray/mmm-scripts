a522f8b0f2c987fcadcaeec617f5404b88c006f3
Mark Murray <mark@murray.xyz>
2019-06-22 21:57:02 +0100

    feat(lint): lint tests

--- src/commands/lint.js
+++ src/commands/lint.js
@@ -2,7 +2,11 @@ const getBinPath = require('../helpers/bin-path')
 const spawn = require('../helpers/spawn')
 
 const lint = async projectRoot => {
-  await spawn(getBinPath('eslint'), ['--ext', '.ts,.tsx', 'src'], projectRoot)
+  await spawn(
+    getBinPath('eslint'),
+    ['--ext', '.ts,.tsx', 'src', 'test'],
+    projectRoot,
+  )
 }
 
 module.exports = lint
