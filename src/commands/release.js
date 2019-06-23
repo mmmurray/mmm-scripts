c964645209583a9f6b81880a69aa47f2a372f16a
Mark Murray <mark@murray.xyz>
2019-06-23 10:26:06 +0100

    fix(release): build before release

--- src/commands/release.js
+++ src/commands/release.js
@@ -1,7 +1,9 @@
 const getBinPath = require('../helpers/bin-path')
 const spawn = require('../helpers/spawn')
+const build = require('./build')
 
 const release = async projectRoot => {
+  await build(projectRoot)
   await spawn(getBinPath('semantic-release'), [], projectRoot)
 }
 
